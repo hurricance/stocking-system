854ad2383574742a6874eac8c5fcd935d2ae4d0c
hurricance <corelution@foxmail.com>
2025-05-10 22:55:04 +0800

    fix: solve cors

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,10 @@
 const Koa = require('koa')
 const { bodyParser } = require("@koa/bodyparser");
+const cors = require('@koa/cors')
 const app = new Koa()
 const router = require('./routes')
 
+app.use(cors())
 app.use(bodyParser())
 app.use(router.routes())
 
